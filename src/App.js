2b90ce97b2f8da7012d023da0c38ab729179fd21
JulianMolina99 <julianmolinac99@gmail.com>
2022-08-25 21:04:05 -0500

    First application with react and Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import {Navbar, NavbarBrand} from 'react-bootstrap'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg='warning'>
+        <div className='container'>
+          <NavbarBrand href="/" className= "text-white">Ristorante Con Fusion</NavbarBrand>
+        </div>
+      </Navbar>
     </div>
+    
   );
 }
 
