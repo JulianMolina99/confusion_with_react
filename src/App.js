3a3f741afb7bc5bdd3038ca0ebf1bc4e52dbd055
JulianMolina99 <julianmolinac99@gmail.com>
2022-08-26 11:47:47 -0500

    Add Media component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import {Navbar, NavbarBrand} from 'react-bootstrap'
+import Menu from './components/MenuComponents';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar bg='warning'>
         <div className='container'>
           <NavbarBrand href="/" className= "text-white">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu/>
     </div>
     
   );
